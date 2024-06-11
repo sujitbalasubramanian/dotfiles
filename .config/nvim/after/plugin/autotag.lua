37bbf8dc964c62bda4d6554e307ab3150aaff61c
Sujit Balasubramanian <sujitbalasubramanianr@gmail.com>
2024-06-11 09:58:30 +0530

    added config for calcure, mpd, ncmpcpp, lf preview with sixel & scripts cleanup.

--- .config/nvim/after/plugin/autotag.lua
+++ .config/nvim/after/plugin/autotag.lua
@@ -1,9 +1,6 @@
-require 'nvim-treesitter.configs'.setup {
-    autotag = {
-        enable = true
-    }
-}
-
 require('nvim-autopairs').setup()
+
 require('nvim-ts-autotag').setup({
+    enable = true,
+    filetypes = { 'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'xml', 'markdown' },
 })
--- .config/nvim/after/plugin/vimwiki.vim
+++
@@ -1,2 +0,0 @@
-let g:vimwiki_global_ext = 0
-let g:vimwiki_list = [{'path': '~/wiki/', 'syntax': 'markdown', 'ext': 'md'}]
